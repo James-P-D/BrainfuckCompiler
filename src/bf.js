6fe0f100660a6934c4bea994d3e430543315cba5
James Dorrington <jdorrington@hotmail.com>
2019-06-29 18:21:32 +0100

    Minor renaming for debug-mode

--- src/bf.js
+++ src/bf.js
@@ -2,29 +2,23 @@ var bfConsts = require('./bf_constants');
 var bfLib = require('./bf_lib');
 var bfCommands = require('./bf_command');
 
-var inputFilename = './examples/alphabet.bf';
-var outputFilename = './examples/alphabet.asm';
-
 var settings = bfLib.parseParameters(process.argv);
-inputFilename = settings.inputFilename;
-outputFilename = settings.outputFilename;
 
-var parse0 = bfLib.readFile(inputFilename);
+var rawInput = bfLib.readFile(settings.inputFilename);
 if (settings.debugMode) {
-    console.log('Parse 1:');
-    console.log(parse0);
+    console.log('Raw input:');
+    console.log(rawInput);
 }
 
-var parse1 = bfLib.removeComments(parse0);
+var parsed = bfLib.removeComments(rawInput);
 if (settings.debugMode) {
-    console.log('Parse 2:');
-    console.log(parse1);
+    console.log('Parsed data:');
+    console.log(parsed);
 }
 
-var commands = bfLib.parse(parse1, 0, 0);
+var commands = bfLib.parse(parsed, 0, 0);
 
-bfLib.compile(commands, outputFilename);
+bfLib.compile(commands, settings.outputFilename);
 
-//bfLib.runFile('./ReturnVal1.exe');
 
 return bfConsts.SUCCESS;
