8892a2fcbab816d00fa62bde82d9fb30788275ac
James Dorrington <jdorrington@hotmail.com>
2019-07-10 22:32:10 +0100

    Minor formatting changes

--- src/footer.asm
+++ src/footer.asm
@@ -1,43 +1,43 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; PROGRAM ENDS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-                    jmp exit                   ; All done!
+                    jmp exit                     ; All done!
 
-getIOHandles:       push STD_OUTPUT_HANDLE     ; _In_ DWORD nStdHandle
-                    call GetStdHandle          ; https://docs.microsoft.com/en-us/windows/console/getstdhandle
-                    mov [consoleOutHandle], eax; Save the output handle
-                    push STD_INPUT_HANDLE      ; _In_ DWORD nStdHandle
-                    call GetStdHandle          ; https://docs.microsoft.com/en-us/windows/console/getstdhandle
-                    mov [consoleInHandle], eax ; Save the input handle
+getIOHandles:       push STD_OUTPUT_HANDLE       ; _In_ DWORD nStdHandle
+                    call GetStdHandle            ; https://docs.microsoft.com/en-us/windows/console/getstdhandle
+                    mov [consoleOutHandle], eax  ; Save the output handle
+                    push STD_INPUT_HANDLE        ; _In_ DWORD nStdHandle
+                    call GetStdHandle            ; https://docs.microsoft.com/en-us/windows/console/getstdhandle
+                    mov [consoleInHandle], eax   ; Save the input handle
                     ret
 
-displayCurrentByte: mov ah, byte ptr [esi]     ; Move the byte from memory into AH register 
-                    mov ioByte, ah             ; Move AH into ioByte
-					push 0                     ; _Reserved_      LPVOID  lpReserved
-                    push offset bytesWritten   ; _Out_           LPDWORD lpNumberOfCharsWritten
-                    push ioByteLength          ; _In_            DWORD   nNumberOfCharsToWrite
-                    push offset ioByte         ; _In_      const VOID *  lpBuffer
-                    push consoleOutHandle      ; _In_            HANDLE  hConsoleOutput
-                    call WriteConsole          ; https://docs.microsoft.com/en-us/windows/console/writeconsole
+displayCurrentByte: mov ah, byte ptr [esi]       ; Move the byte from memory into AH register 
+                    mov ioByte, ah               ; Move AH into ioByte
+					push 0                       ; _Reserved_      LPVOID  lpReserved
+                    push offset bytesWritten     ; _Out_           LPDWORD lpNumberOfCharsWritten
+                    push ioByteLength            ; _In_            DWORD   nNumberOfCharsToWrite
+                    push offset ioByte           ; _In_      const VOID *  lpBuffer
+                    push consoleOutHandle        ; _In_            HANDLE  hConsoleOutput
+                    call WriteConsole            ; https://docs.microsoft.com/en-us/windows/console/writeconsole
                     ret
 
-readCurrentByte:    push -1                    ; _In_opt_        LPVOID  pInputControl
-                    push offset bytesRead      ; _Out_           LPDWORD lpNumberOfCharsRead
-                    push 1                     ; _In_            DWORD   nNumberOfCharsToRead
-                    push offset ioByte         ; _Out_           LPVOID  lpBuffer
-                    push consoleInHandle       ; _In_            HANDLE  hConsoleInput
-                    call ReadConsole           ; https://docs.microsoft.com/en-us/windows/console/readconsole
-                    mov ah, ioByte             ; Move the byte read into AH register
-                    mov byte ptr [esi], ah     ; Move the AH register into our memory buffer
+readCurrentByte:    push -1                      ; _In_opt_        LPVOID  pInputControl
+                    push offset bytesRead        ; _Out_           LPDWORD lpNumberOfCharsRead
+                    push 1                       ; _In_            DWORD   nNumberOfCharsToRead
+                    push offset ioByte           ; _Out_           LPVOID  lpBuffer
+                    push consoleInHandle         ; _In_            HANDLE  hConsoleInput
+                    call ReadConsole             ; https://docs.microsoft.com/en-us/windows/console/readconsole
+                    mov ah, ioByte               ; Move the byte read into AH register
+                    mov byte ptr [esi], ah       ; Move the AH register into our memory buffer
                     ret
 
-; readCurrentByte2:   push offset bytesRead      ; Gah! This sort-of works. The problem with ReadConsole is that it doesnt return until <ENTER>
-;                     push 1                     ; is pressed. The problem with ReadConsoleInput is that it returns when *any* key is pressed
-;                     push offset displayByte    ; including <SHIFT>, <ALT>, <CAPS-LOCK> etc. ReadConsoleInput is too fine for our needs,
-;                     push consoleInHandle       ; but depressingly ReadConsole isnt fine enough. :(
-;                     call ReadConsoleInput      ; https://docs.microsoft.com/en-us/windows/console/readconsoleinput
+; readCurrentByte2:   push offset bytesRead        ; Gah! This sort-of works. The problem with ReadConsole is that it doesnt return until <ENTER>
+;                     push 1                       ; is pressed. The problem with ReadConsoleInput is that it returns when *any* key is pressed
+;                     push offset displayByte      ; including <SHIFT>, <ALT>, <CAPS-LOCK> etc. ReadConsoleInput is too fine for our needs,
+;                     push consoleInHandle         ; but depressingly ReadConsole isnt fine enough. :(
+;                     call ReadConsoleInput        ; https://docs.microsoft.com/en-us/windows/console/readconsoleinput
 
-exit:              push 0                      ; Exit code zero for success
-                   call ExitProcess            ; https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-exitprocess
+exit:              push 0                        ; Exit code zero for success
+                   call ExitProcess              ; https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-exitprocess
 
 end start
--- src/header.asm
+++ src/header.asm
@@ -9,23 +9,23 @@ includelib c:\\masm32\\lib\\kernel32.lib
 includelib c:\\masm32\\m32lib\\masm32.lib
 
 .data
-STD_OUTPUT_HANDLE   equ -11                    ; https://docs.microsoft.com/en-us/windows/console/getstdhandle
-STD_INPUT_HANDLE    equ -10                    ; https://docs.microsoft.com/en-us/windows/console/getstdhandle
-ioByte              db 0                       ; Our input/output byte
-ioByteLength        equ $ - offset ioByte      ; Length of ioByte
-BUFFER_SIZE         equ 10000                  ; Brainfuck buffer size
-bufferStart         equ $                      ; Start position of buffer
-buffer              db BUFFER_SIZE dup(0)      ; Our 10000-byte brainfuck buffer, initialised to zero
+STD_OUTPUT_HANDLE   equ -11                      ; https://docs.microsoft.com/en-us/windows/console/getstdhandle
+STD_INPUT_HANDLE    equ -10                      ; https://docs.microsoft.com/en-us/windows/console/getstdhandle
+ioByte              db 0                         ; Our input/output byte
+ioByteLength        equ $ - offset ioByte        ; Length of ioByte
+BUFFER_SIZE         equ 10000                    ; Brainfuck buffer size
+bufferStart         equ $                        ; Start position of buffer
+buffer              db BUFFER_SIZE dup(0)        ; Our 10000-byte brainfuck buffer, initialised to zero
 
 .data?
-consoleOutHandle    dd ?                       ; Our ouput handle (currently undefined)
-consoleInHandle     dd ?                       ; Our input handle (currently undefined)
-bytesWritten        dd ?                       ; Number of bytes written to output (currently undefined)
-bytesRead           dd ?                       ; Number of bytes written to input (currently undefined)
+consoleOutHandle    dd ?                         ; Our ouput handle (currently undefined)
+consoleInHandle     dd ?                         ; Our input handle (currently undefined)
+bytesWritten        dd ?                         ; Number of bytes written to output (currently undefined)
+bytesRead           dd ?                         ; Number of bytes written to input (currently undefined)
 
 .code
-start:              call getIOHandles          ; Get the input/output handles
-                    mov esi, bufferStart       ; 
+start:              call getIOHandles            ; Get the input/output handles
+                    mov esi, bufferStart         ; 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; PROGRAM STARTS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
